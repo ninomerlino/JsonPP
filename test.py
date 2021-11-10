c5e7bcb455dbd3e60af68a66172ca83ed9c85224
ninomerlino <lorenzomerlino9@gmail.com>
2021-11-10 17:23:27 +0100

    Json is now a namespace JsonData is the Class used to interact with the Json

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-data = None
-with open("file.json", "r") as file:
-    data = json.loads(file.read())
-data.append("This json was modfied successfuly")
-with open("filedump.json", "w") as file:
-    file.write(json.dumps(data))
--- times.md
+++
@@ -1,6 +0,0 @@
-# 1000 instances
-### time in ms
-| version | min | avg | max |
-|---|---|---|---|
-| python | 11 | 16 | 37 |
-| crap c++ | 1 | 2 | 16 |
